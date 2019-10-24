e0d791f557a057122d93e566b164d399b1af5b65
mikeygbd <mikey.soares@gmail.com>
2019-10-24 09:58:26 -0400

    wired up two buttons one for Signin screen and one to the mainFlow

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -3,14 +3,17 @@ import {
   View,
   Text,
   StyleSheet,
+  Button
 } from 'react-native';
 
-const SignupScreen = ({}) => (
-  <View style={styles.container}>
+const SignupScreen = ({ navigation }) => {
+  return <>
     <Text style={{fontSize: 48}}>I'm SignupScreen</Text>
-  </View>
-);
+    <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')} />
+    <Button title="Go to mainFlow" onPress={() => navigation.navigate('mainFlow')} />
 
+  </>
+}
 export default SignupScreen;
 
 const styles = StyleSheet.create({
