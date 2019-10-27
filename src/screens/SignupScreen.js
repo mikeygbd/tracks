4d1924bc381a4552516eacba7159d84e426776eb
mikeygbd <mikey.soares@gmail.com>
2019-10-27 19:51:38 -0400

    added Spacer to spread out the the inputs and buttons

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import {
   View,
-  Text,
   StyleSheet,
-  Button
+
 } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements'
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
   return <>
-    <Text style={{fontSize: 48}}>I'm SignupScreen</Text>
-    <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')} />
-    <Button title="Go to mainFlow" onPress={() => navigation.navigate('mainFlow')} />
+    <Spacer>
+      <Text h3>Sign Up for Tracker</Text>
+    </Spacer>
+      <Input label="Email" />
+    <Spacer />
+      <Input label="Password" />
+    <Spacer>
+      <Button title="Sign Up" onPress={() => {}} />
+    </Spacer>
+    <Spacer>
+      <Button title="Sign In" onPress={() => navigation.navigate('Signin')} />
+    </Spacer>
+    <Spacer>
+      <Button title="Go to mainFlow" onPress={() => navigation.navigate('mainFlow')} />
+    </Spacer>
+
+
 
   </>
 }
