26fcb2e5d7375fa528c6d3f63b50d0755aa6599a
mikeygbd <mikey.soares@gmail.com>
2019-11-04 07:55:45 -0500

    fixed typo of array instead of an object

--- src/navigationRef.js
+++ src/navigationRef.js
@@ -1,15 +1,15 @@
 import { NavigationActions } from 'react-navigation'
 let navigator;
 
-export const setNavigator = (nav) => {
+export const setNavigator = nav => {
   navigator = nav
 }
 
 export const navigate = (routeName, params) => {
   navigator.dispatch(
-    NavigationActions.navigate([
+    NavigationActions.navigate({
       routeName,
       params
-    ])
+    })
   )
 }
