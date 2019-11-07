065351a16d7123ced193f5aabff079ba94fd047a
mikeygbd <mikey.soares@gmail.com>
2019-11-07 07:45:14 -0500

    imported usContext and Context and destructured state and signin. and used them on the errorMessage and onSubmit

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   View,
   Text,
@@ -6,15 +6,18 @@ import {
 } from 'react-native';
 import AuthForm from '../components/AuthForm'
 import NavLink from '../components/NavLink'
-
+import { Context } from '../context/AuthContext'
 
 const SigninScreen = ({ }) => {
+
+  const { state, signin } = useContext(Context)
+
   return (
   <View style={styles.container}>
     <AuthForm
       headerText="Sign In to Your Account"
-      errorMessage=""
-      onSubmit={() => {}}
+      errorMessage={state.errorMessage}
+      onSubmit={signin}
       submitButtonText="Sign In"
     />
     <NavLink
