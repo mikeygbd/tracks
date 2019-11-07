b7d790b8749412e66a5b755dba222e11c5c552be
mikeygbd <mikey.soares@gmail.com>
2019-11-07 07:30:23 -0500

    imported AuthForm and Navlink and returned them with a slightly diffrent text than sign up

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -4,17 +4,36 @@ import {
   Text,
   StyleSheet,
 } from 'react-native';
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
 
-const SigninScreen = ({}) => (
+
+const SigninScreen = ({ }) => {
+  return (
   <View style={styles.container}>
-    <Text style={{fontSize: 48}}>I'm SigninScreen</Text>
+    <AuthForm
+      headerText="Sign In to Your Account"
+      errorMessage=""
+      onSubmit={() => {}}
+      submitButtonText="Sign In"
+    />
+    <NavLink
+      text="Don't have an account? Sign up instead."
+      routeName="Signup"
+    />
   </View>
 );
-
+}
 export default SigninScreen;
+
+SigninScreen.navigationOptions = {
+  header: null
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 120,
   },
 });
