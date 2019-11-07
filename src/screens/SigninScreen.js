6512bae1e8410745b27b74a498ef10f20fc9bd8e
mikeygbd <mikey.soares@gmail.com>
2019-11-07 08:05:04 -0500

    imported NavigationEvents and imported the clearErrorMessage from Context and called an onWillBlur in NavigationEvents with a clearErrorMessage

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -4,16 +4,20 @@ import {
   Text,
   StyleSheet,
 } from 'react-native';
+import { NavigationEvents } from 'react-navigation'
 import AuthForm from '../components/AuthForm'
 import NavLink from '../components/NavLink'
 import { Context } from '../context/AuthContext'
 
 const SigninScreen = ({ }) => {
 
-  const { state, signin } = useContext(Context)
+  const { state, signin, clearErrorMessage } = useContext(Context)
 
   return (
   <View style={styles.container}>
+    <NavigationEvents
+      onWillBlur={clearErrorMessage}
+     />
     <AuthForm
       headerText="Sign In to Your Account"
       errorMessage={state.errorMessage}
