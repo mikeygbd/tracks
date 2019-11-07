2f95a0afabc105855584801e14e8b99b972619e0
mikeygbd <mikey.soares@gmail.com>
2019-11-07 08:03:51 -0500

    created clearErrorMessage to clear it when navigating from signup to sign in

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -9,10 +9,16 @@ const authReducer = (state, action) => {
     return { ...state, errorMessage: action.payload }
     case 'signin':
     return { errorMessage: '', token: action.payload}
+    case 'clear_error_message':
+    return { ...state, errorMessage: '' }
     default:
     return state
 
   }
+}
+
+const clearErrorMessage = dispatch => () => {
+  dispatch({type: 'clear_error_message' })
 }
 
 const signup = (dispatch) => async ({ email, password }) => {
@@ -47,6 +53,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {signin, signout, signup},
+  {signin, signout, signup, clearErrorMessage},
   { token: null, errorMessage: '' }
 )
