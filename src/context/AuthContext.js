a37fafec9d91a6f8c0028c7e5f523c974ef8ef78
mikeygbd <mikey.soares@gmail.com>
2019-11-07 07:46:54 -0500

    created the post request for sign in same as sign up and combined them in the same action type of sign in because they do the same thing

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,9 +7,8 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
     return { ...state, errorMessage: action.payload }
-    case 'signup':
+    case 'signin':
     return { errorMessage: '', token: action.payload}
-
     default:
     return state
 
@@ -20,21 +19,25 @@ const signup = (dispatch) => async ({ email, password }) => {
   try{
     const response = await trackerApi.post('/signup', { email, password })
     await AsyncStorage.setItem('token', response.data.token)
-    dispatch({ type: 'signup', payload: response.data.token })
+    dispatch({ type: 'signin', payload: response.data.token })
     navigate('TrackList')
   } catch (err) {
     dispatch({ type: 'add_error', payload: 'Something went wrong with Sign Up'})
   }
 }
 
-const signin = (dispatch) => {
-  return ({ email, password }) => {
-    // make api request to signin with that email and Password
-    // handle success
-    // if signin fails we need to reflect an error message
+const signin = (dispatch) => async ({ email, password }) => {
+    try{
+      const response = await trackerApi.post('/signin', { email, password })
+      await AsyncStorage.setItem('token', response.data.token)
+      dispatch({ type: 'signin', payload: response.data.token })
+      navigate('TrackList')
+    } catch (err) {
+      dispatch({ type: 'add_error', payload: 'Something went wrong with Sign In'})
 
+    }
   }
-}
+
 
 const signout = (dispatch) => {
   return () => {
