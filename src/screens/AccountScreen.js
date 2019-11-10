31407d927ee250527e2efda054b61ecf7bde3cba
mikeygbd <mikey.soares@gmail.com>
2019-11-10 12:39:42 -0500

    imported signout from AuthContext and added a button to call signout

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   View,
   Text,
   StyleSheet,
 } from 'react-native';
+import { Button } from 'react-native-elements'
+import Spacer from '../components/Spacer'
+import {Context as AuthContext} from '../context/AuthContext'
 
-const AccountScreen = ({}) => (
-  <View style={styles.container}>
+const AccountScreen = ({}) => {
+
+  const {signout} = useContext(AuthContext)
+
+  return (
+  <>
     <Text style={{fontSize: 48}}>I'm AccountScreen</Text>
-  </View>
-);
-
+    <Spacer>
+    <Button title="Sign Out" onPress={signout} />
+    </Spacer>
+  </>
+  );
+}
 export default AccountScreen;
 
 const styles = StyleSheet.create({
