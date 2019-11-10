6347c4c155339e141f1ad740c72ed87dc085fa23
mikeygbd <mikey.soares@gmail.com>
2019-11-10 08:47:26 -0500

    inserted a use effect that calls the tryLocalSignin as soon as the component first renders

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import {
   View,
   StyleSheet
@@ -9,8 +9,11 @@ import NavLink from '../components/NavLink'
 import { NavigationEvents } from 'react-navigation'
 
 const SignupScreen = ({ navigation }) => {
-  const { state, signup, clearErrorMessage } = useContext(AuthContext)
+  const { state, signup, clearErrorMessage, tryLocalSignin } = useContext(AuthContext)
 
+  useEffect(() => {
+    tryLocalSignin()
+  }, [])
   return (
   <View style={styles.container}>
     <NavigationEvents
