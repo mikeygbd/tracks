28a1697fa7731aaeda86c008eb412d76ec8a9b1c
mikeygbd <mikey.soares@gmail.com>
2019-11-10 12:38:56 -0500

    removed token in signout and navigated to login flow

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,6 +11,8 @@ const authReducer = (state, action) => {
     return { errorMessage: '', token: action.payload}
     case 'clear_error_message':
     return { ...state, errorMessage: '' }
+    case 'signout':
+    return { token: null, errorMessage: '' }
     default:
     return state
 
@@ -55,11 +57,12 @@ const signin = (dispatch) => async ({ email, password }) => {
   }
 
 
-const signout = (dispatch) => {
-  return () => {
-
+const signout = dispatch => async () => {
+  await AsyncStorage.removeItem('token')
+  dispatch({ type: 'signout'})
+  navigate('loginFlow')
   }
-}
+
 
 export const { Provider, Context } = createDataContext(
   authReducer,
