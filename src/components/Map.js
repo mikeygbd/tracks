6bbccc5a18bf182d0096154953b2d71ad77a893b
mikeygbd <mikey.soares@gmail.com>
2019-11-12 09:24:13 -0500

    created basic template for map

---
+++ src/components/Map.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+const Map = ({}) => (
+  <>
+    <Text>I'm Map</Text>
+  </>
+);
+
+export default Map;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
