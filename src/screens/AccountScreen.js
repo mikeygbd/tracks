006b2c3e45ae354ec5d237b4271496a3ba25ee14
mikeygbd <mikey.soares@gmail.com>
2019-11-12 09:14:50 -0500

    Wrapped the screen in SafeAreaView to keep the header from going up behind the time and battery dispalay

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -7,18 +7,19 @@ import {
 import { Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
 import {Context as AuthContext} from '../context/AuthContext'
+import { SafeAreaView } from 'react-navigation'
 
 const AccountScreen = ({}) => {
 
   const {signout} = useContext(AuthContext)
 
   return (
-  <>
+  <SafeAreaView forceInset={{ top: 'always'}}>
     <Text style={{fontSize: 48}}>I'm AccountScreen</Text>
     <Spacer>
     <Button title="Sign Out" onPress={signout} />
     </Spacer>
-  </>
+  </SafeAreaView>
   );
 }
 export default AccountScreen;
