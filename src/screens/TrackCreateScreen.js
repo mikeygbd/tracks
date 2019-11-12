fbc4de17666ca091c3ae1e2bfcb88daf79fb318c
mikeygbd <mikey.soares@gmail.com>
2019-11-12 10:35:40 -0500

    imported requestPermissionsAsync to request to the user to allow to use their location. created a function to start watching to catch weather or not the permission is valid or not

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
 } from 'react-native';
 import Map from '../components/Map'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-navigation'
+import { requestPermissionsAsync } from 'expo-location'
 
-const TrackCreateScreen = ({}) => (
+
+const TrackCreateScreen = ({}) => {
+
+  const [err, setErr] = useState(null)
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync()
+    } catch (e) {
+      setErr(e)
+    }
+  }
+
+  useEffect(() => {
+    startWatching()
+  }, [])
+
+  return (
   <SafeAreaView forceInset={{ top: 'always' }}>
     <Text h2>Create a Track</Text>
     <Map />
+    {err ? <Text> Please enable location services</Text> : null}
   </SafeAreaView>
-);
+  )
+}
 
 export default TrackCreateScreen;
 
