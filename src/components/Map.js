28a925ef6bcc0c3f4dcefdda3c0c285efbbb25ff
mikeygbd <mikey.soares@gmail.com>
2019-11-12 09:35:08 -0500

    imported react-native-maps and inserted a mapview into the screen with an initial region of the apple headquarters

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,11 +3,21 @@ import {
   Text,
   StyleSheet,
 } from 'react-native';
+import MapView from 'react-native-maps'
+
 
 const Map = ({}) => (
-  <>
-    <Text>I'm Map</Text>
-  </>
+
+    <MapView
+    style={styles.map}
+    initialRegion={{
+      latitude: 37.33233,
+      longitude: -122.03121,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01
+    }}
+     />
+
 );
 
 export default Map;
@@ -16,4 +26,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  map: {
+    height: 300
+  }
 });
