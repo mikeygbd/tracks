f6e10f051cbcc87a7132ebbd473dce4fddcc95c6
mikeygbd <mikey.soares@gmail.com>
2019-11-13 08:37:55 -0500

    created a location reducer and started making start and stop recording and add location and exported them with initial state of recording locations and currentLocations

---
+++ src/context/LocationContext.js
@@ -0,0 +1,24 @@
+import createDataContext from './createDataContext'
+
+const locationReducer = () => {
+  switch (action.type) {
+    default:
+    return state
+  }
+}
+
+const startRecording = dispatch => () => {
+
+}
+const stopRecording = dispatch => () => {
+
+}
+const addLocation = dispatch => () => {
+
+}
+
+export const { Context, Provider } = createDataContext(
+  locationReducer,
+  { startRecording, stopRecording, addLocation },
+  { recording: false, locations: [], currentLocation: null }
+)
