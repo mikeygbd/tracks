00eadfc0fea1a019e113d95c338c0a0a87645384
mikeygbd <mikey.soares@gmail.com>
2019-11-13 18:31:21 -0500

    added circle to MapView with props giving it our currentLocation.coords

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,7 @@ import {
   StyleSheet,
   ActivityIndicator
 } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps'
+import MapView, { Polyline, Circle } from 'react-native-maps'
 import { Context as LocationContext } from '../context/LocationContext'
 
 
@@ -24,12 +24,15 @@ const Map = ({}) => {
       latitudeDelta: 0.01,
       longitudeDelta: 0.01
     }}
-    region={{
-      ...currentLocation.coords,
-      latitudeDelta: 0.01,
-      longitudeDelta: 0.01
-    }}
+
+     >
+     <Circle
+      center={currentLocation.coords}
+      radius={30}
+      strokeColor="rgba(158, 158, 255, 1.0)"
+      fillColor="rgba(158, 158, 255, 0.4)"
      />
+     </MapView>
 
 
    );
