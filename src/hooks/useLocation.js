4d17ee60aed1ca56d903de4ea2224985c55efacd
mikeygbd <mikey.soares@gmail.com>
2019-11-14 07:46:20 -0500

    added const subscriber to watchPositionAsync

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -8,7 +8,7 @@ export default (callback) => {
   const startWatching = async () => {
     try {
       await requestPermissionsAsync()
-      await watchPositionAsync({
+      const subscriber = await watchPositionAsync({
         accuracy: Accuracy.BestForNavigation,
         timeInterval: 1000,
         distanceInterval: 10
