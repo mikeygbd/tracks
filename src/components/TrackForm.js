e02ee5f00292c65caefe6c6722854b3077d09da1
mikeygbd <mikey.soares@gmail.com>
2019-11-16 00:46:33 -0500

    Created a track form with an input and a start recording button

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { Input, Button } from 'react-native-elements'
+import Spacer from './Spacer'
+const TrackForm = ({}) => (
+  <>
+  <Spacer>
+  <Input placeholder="Enter name" />
+  </Spacer>
+  <Button title="Start Recording" />
+  </>
+);
 
+export default TrackForm;
