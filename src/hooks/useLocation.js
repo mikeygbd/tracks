811291313822316dcb49fe49da9e8ef2355e6211
mikeygbd <mikey.soares@gmail.com>
2019-11-19 13:15:27 -0500

    inserted start Watching into useEffect and deleted subscriber state and just used a let variable

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -5,30 +5,31 @@ import  { Accuracy, requestPermissionsAsync, watchPositionAsync} from 'expo-loca
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null)
 
-  const [subscriber, setSubscriber] = useState(null)
-
-  const startWatching = async () => {
-    try {
-      await requestPermissionsAsync()
-      const sub = await watchPositionAsync({
-        accuracy: Accuracy.BestForNavigation,
-        timeInterval: 1000,
-        distanceInterval: 10
-      },
-       callback
-    )
-    setSubscriber(sub)
-    } catch (e) {
-      setErr(e)
-    }
-  }
 
   useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        await requestPermissionsAsync()
+        const subscriber = await watchPositionAsync({
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10
+        },
+         callback
+      )
+      } catch (e) {
+        setErr(e)
+      }
+    }
+
     if (shouldTrack) {
       startWatching()
     } else {
+      if (subscriber) {
       subscriber.remove()
-      setSubscriber(null)
+    }
+      subscriber = null
     }
 
     return () => {
