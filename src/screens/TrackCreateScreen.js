bd2dcf18fc1c266d8a42af2204169b55d9e3fbda
mikeygbd <mikey.soares@gmail.com>
2019-11-30 09:25:41 -0500

    added a plus icon to addTrack

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -5,10 +5,11 @@ import {
 import Map from '../components/Map'
 import { Text } from 'react-native-elements'
 import { SafeAreaView, NavigationEvents, withNavigationFocus } from 'react-navigation'
-import '../_mockLocation'
+// import '../_mockLocation'
 import { Context as LocationContext } from '../context/LocationContext'
 import useLocation from '../hooks/useLocation'
 import TrackForm from '../components/TrackForm'
+import { FontAwesome } from '@expo/vector-icons'
 
 const TrackCreateScreen = ({ isFocused }) => {
 
@@ -26,6 +27,11 @@ const TrackCreateScreen = ({ isFocused }) => {
     <TrackForm />
   </SafeAreaView>
   )
+}
+
+TrackCreateScreen.navigationOptions ={
+    title: 'Add Track',
+    tabBarIcon: <FontAwesome name="plus" size={20} />
 }
 
 export default withNavigationFocus(TrackCreateScreen);
