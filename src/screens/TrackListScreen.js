97a042afdd29e4d8f1f9a15d297fff7fd61852be
mikeygbd <mikey.soares@gmail.com>
2019-11-30 09:08:10 -0500

    got FetchTracks from trackContext and added a NavigationEvent to fetchTracks when the screen is rendered. Then created a Flatlist with al the tracks with a touchable opacity taking you to the track detail screen with the id of the track

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
-  View,
+  FlatList,
   Text,
   StyleSheet,
-  Button
+  TouchableOpacity
 } from 'react-native';
+import {NavigationEvents} from 'react-navigation'
+import {Context as TrackContext} from '../context/TrackContext'
+import { ListItem } from 'react-native-elements'
 
-const TrackListScreen = ({ navigation }) => (
+const TrackListScreen = ({ navigation }) => {
+
+  const { state, fetchTracks } = useContext(TrackContext)
+  console.log(state);
+  return (
   <>
+    <NavigationEvents onWillFocus={fetchTracks} />
     <Text style={{fontSize: 48}}>I'm TrackListScreen</Text>
-    <Button title="Go to TrackDetail" onPress={() => navigation.navigate('TrackDetail')} />
+    <FlatList
+    data={state}
+    keyExtractor={item => item._id}
+    renderItem={({ item })=> {
+      return <TouchableOpacity onPress={() => navigation.navigate('TrackDetail', { _id: item._id })}>
+        <ListItem chevron title={item.name} />
+      </TouchableOpacity>
+    }}
+    />
 
   </>
-);
+  );
+}
 
 export default TrackListScreen;
 
